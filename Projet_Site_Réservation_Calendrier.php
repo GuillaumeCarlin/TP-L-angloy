1b941d7581df7937fb2e27a1ad2f86fff6b9188a
g.bouvier <g.bouvier@eleve.leschartreux.net>
2022-01-28 10:37:23 +0100

    connexion bdd

--- Projet_Site_Réservation_Calendrier.php
+++ Projet_Site_Réservation_Calendrier.php
@@ -20,7 +20,27 @@
 
     </br>
     </br>
+    <?php
 
+        ///connexion à mysql et selection de la base de données//
+        $connexion = mysqli_connect("localhost","root","","bdd_prixy");
+        if ($connexion) { 
+            echo 'Connexion au serveur réussie';
+            $BDD = mysqli_select_db($connexion,'bdd_prixy');
+            if ($BDD) {
+                echo '<div id="bdd">Base de données sélectionnée</div>';
+                
+            }
+            else{ 
+                    echo '<div id="bdd">Echec de la sélection de la base</div>'; 
+            }
+        } 
+        else{ 
+            echo '<div id="srv">Erreur lors de la connexion</div>';
+        }
+    
+
+    ?>
 
     <body>
 
