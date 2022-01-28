a133eb37803627501ae5159db3781f6c7eb0da43
Mon Nom d'utilisateurGuillaumeCarlin <g.carlin@eleve.leschartreux.net>
2022-01-28 09:13:36 +0100

    Création Page Compte

---
+++ Projet_Site_Réservation_Page_Compte.php
@@ -0,0 +1,25 @@
+<html>
+    <link rel="stylesheet" href="Projet_Site_Réservation_Page_Connexion.css">
+    <body class="body">
+        <fieldset class="fieldset">
+            </br>
+            </br>
+            <texte class="titre_Compte">Nouveau compte</texte>
+            </br>
+            <input type="texte" class="bouton_Compte" placeholder="Utilisateur" required>
+            </br>
+            <input type="password" class="bouton_Compte" placeholder="Mot de Passe" required>
+            </br>
+            <input type="password" class="bouton_Compte" placeholder="Confirmer le mot de passe" required>
+            </br>
+            </br>
+            </br>
+            <div class="check">
+            <text class="admin_Compte">Administrateur </text> <input type="checkbox" id="test3"/><label for="test3"><span class="ui"></span>
+            </div>
+            <input type="submit" class="boutonNvCpt_Compte" value="Créer un compte">
+
+        </fieldset>
+
+    </body>
+</html>
