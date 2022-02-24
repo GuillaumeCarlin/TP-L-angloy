611924f0b5942e4e4dad7c7625e75bba17375d7b
Nathan Richard-Ducros <n.richardducros@eleve.leschartreux.net>
2022-02-24 09:50:08 +0100

    feat

---
+++ Calendrier/update.php
@@ -0,0 +1,29 @@
+<?php
+
+//update.php
+
+$connect = new PDO('mysql:host=localhost;dbname=bdd_prixy', 'root', '');
+
+if(isset($_POST["id"]))
+{
+ $query = "
+ UPDATE events 
+ SET title=:title, start_event=:start_event, end_event=:end_event, descriptionEvent=:descriptionEvent, participant=:participant, IDSalle=:IDSalle, UTILNomUtilisateur=:UTILNomUtilisateur  
+ WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':title'  => $_POST['title'],
+   ':start_event' => $_POST['start'],
+   ':end_event' => $_POST['end'],
+   ':id'   => $_POST['id'],
+   ':descriptionEvent' => $_POST['descriptionEvent'],
+   ':participant' => $_POST['participant'],
+   ':IDSalle' => $_POST['IDSalle'],
+   ':UTILNomUtilisateur' => $_POST['UTILNomUtilisateur']
+  )
+ );
+}
+
+?>
