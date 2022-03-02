1b2eafad5c2ed9b81130bd35b2a67b943e7cbcdd
g.bouvier <g.bouvier@eleve.leschartreux.net>
2022-03-02 17:59:23 +0100

    page creation de compte final

--- Projet_Site_Reservation_Page_Compte.php
+++ Projet_Site_Reservation_Page_Compte.php
@@ -31,86 +31,52 @@
                 <?php
                 // ini_set("display_errors","off");
                 
-                if ($_SERVER["REQUEST_METHOD"] == "POST") { // implemente les valeurs dans $_POST
+                if ($_SERVER["REQUEST_METHOD"] == "POST") { // implemente les valeurs dans $_POST si la methode est la bonne
                     $NomUtilisateur = $_POST["Utilisateur"];
                     $mdp = $_POST['mdp'];
                     $mdpC = $_POST['mdpC'];
-                    if ($_POST['declare_admin']){
-                        $declare_admin = $_POST['declare_admin'];
+                   
+                    if ($_POST['admin']==True){
+                        $declare_admin=1;
                     }
                     
-                    if(count($_POST)>=3)
-
-                        /*Vérification du mot de passe */
-                        if ($mdp == $mdpC){
-                            /* Si le mot de passe est bon le compte est créer*/
-                            $requete = "INSERT INTO utilisateur(UTILNomUtilisateur, UTILMotDePasse) VALUES('$NomUtilisateur', '$mdp')";
-                            $resultat = mysqli_query($connexion,$requete) or die(mysqli_error($connexion)); 
-                            
-                            if($declare_admin){
-                                $requete="INSERT INTO utilisateur (UTILNomUtilisateur, UTILMotDePasse, UTILAdmin) VALUES ('$NomUtilisateur', '$mdp', '1');";
-                                $larequete = mysqli_query($connexion,$requete);
-                                echo"utilisateur crée";
-                            }
-                            else{
-                                $requete = "INSERT INTO utilisateur(UTILNomUtilisateur, UTILMotDePasse) VALUES('$NomUtilisateur', '$mdp')";
-                                $larequete = mysqli_query($connexion,$requete);
-                                echo"utilisateur crée";
-                            }
-                        } 
-                        else{
-                            echo"<div class=erreur_crea_mdp><strong> Le Nom d'utilisateur existe déja </strong></div>";
-                        }
-                    }
-                
-
-                
-                /*
-                if (count($_POST)>=3){
-                    $connexion = mysqli_connect("localhost","root","","bdd_formulaire");
-                    $requete = mysqli_query($connexion,"SELECT count(*) FROM 'utilisateur' where 'UTILNomUtilisateur' ='NomUtilisateur';");
-                    $nbiteration=mysqli_affected_rows($connexion);
-                    if($nbiteration>=1){
-                        echo"<div class=erreur_crea_nomutilisateur><strong> Le Nom d'utilisateur existe déja </strong></div>";
-                        // $resultat=mysqli_fetch_array($requete,MYSQLI_NUM);
-                        // $compte=$resultat['count(*)'];
-                    }
-                    
-                    /* Verification si le nom d'utilisateur existe deja dans la base */
-                /*
-                    if($compte){
-                        echo"<div class=erreur_crea_nomutilisateur><strong> Le Nom d'utilisateur existe déja </strong></div>";
-                    }
                 }
-                */
-               
-            
-                /*
-                $connexion = mysqli_connect("localhost","root","","bdd_formulaire");
-                if (count($_POST)>=3){
-                    if ($connexion) { 
-                        $BDD = mysqli_select_db($connexion,'bdd_prixy');
-                        if ($BDD) {
-                            ///////////////////////////////////////////////////////////////////////
-                            ///////////////////////////////////////////////////////////////////////
-                            $lestatutconnexion=false;
-                            $requete = mysqli_query($connexion,"SELECT count(*) FROM utilisateur where UTILNomUtilisateur ='".$utilisateur."' and UTILMotDePasse = '".$mdp."';");
-                            $resultat=mysqli_fetch_array($requete);
-                            $compte=$resultat['count(*)'];
-                            
-                            // md5() --> hachage
-                            if ($compte != 0){
-                                echo "<div class=aligement_milieu_connexion> <strong> Connexion en cours ... </strong></div>";
+
+                $connexion = mysqli_connect("localhost","root","","bdd_prixy");
+                if ($connexion) { 
+                    $BDD = mysqli_select_db($connexion,'bdd_prixy');
+                    if ($BDD) {
+                        if(count($_POST)>=3){
+
+                            /*Vérification du mot de passe */
+                            if ($mdp == $mdpC){
+                                /* Si le mot de passe est bon le compte est créer*/
+                                
+                                
+                                
+                                if($declare_admin==1){
+                                    
+                                    $requete="INSERT INTO utilisateur (UTILNomUtilisateur, UTILMotDePasse, UTILAdmin) VALUES ('$NomUtilisateur', '$mdp', '$declare_admin');";
+                                    $larequete = mysqli_query($connexion,$requete);
+                                    echo"utilisateur crée";
+                                }
+                                else{
+                                    $requete = "INSERT INTO utilisateur(UTILNomUtilisateur, UTILMotDePasse) VALUES('$NomUtilisateur', '$mdp')";
+                                    $larequete = mysqli_query($connexion,$requete);
+                                    echo"utilisateur crée";
+                                }
                                 header('Location: calendrier.html');
-                            }
-                            else{
-                                echo"<div class=erreurconnexion><strong> Nom d'utilisateur ou mot de passe incorrecte </strong></div>";
                                 
                             } 
-                            
+                            else{
+                                echo"<div class=erreur_crea_mdp><strong>Erreur dans le mot de passe</strong></div>";
+                            }
                         }
-                    }    
-                }*/
+                    }
+                }
+                
+
+                
                 ?>
             </fieldset>
         </form>
