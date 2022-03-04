5a8ae5ebc8ebfa70ac679c07eab0858c1cad9d01
Nathan Richard-Ducros <n.richardducros@eleve.leschartreux.net>
2022-03-04 10:46:00 +0100

    a

--- Calendrier/test.php
+++ Calendrier/test.php
@@ -1,26 +1,10 @@
 <?php
 
-$date = date('d-m-y h:i:s');
-$query = "UPDATE `events` SET `horodatage` = '04-03-22 07:57:14' WHERE `events`.`id` = 1;";
-$result = mysqli_query($mysqli, $query);
+session_start();
 
-$con = mysqli_connect('localhost','root','');
-if ($con) {
-    $connectdb = mysqli_select_db($con, 'bdd_prixy');
-    if ($connectdb) {
-        $query = "UPDATE `events` SET `horodatage` = '$date' WHERE `events`.`id` = 1;";
-        $result = mysqli_query($mysqli, $query);
-    }
-    else {
-        echo 'Erreur de connexion à la base de donnée';
-    }
-}
-else {
-    echo 'Erreur de connexion au serveur';
-}
-
-
-
+$id = $_SESSION['id'];
+echo $id;
+echo 'test';
 
 ?>
 
