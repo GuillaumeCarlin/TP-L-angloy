f738d234c24845c4bdb70e2e051f6fe865e60602
Evan PREVOST <e.prevost@eleve.leschartreux.net>
2022-03-11 08:24:07 +0100

    yeah

--- z_config.php
+++ z_config.php
@@ -2,7 +2,7 @@
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
-$dbname = "base_prixy"; /* Database name */
+$dbname = "bdd_prixy"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // Check connection
